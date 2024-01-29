0a1121cff7ed343c4a3f14d8a5bbbe02ce3cd5e8
Adam Camerer <ajc3xc@umsystem.edu>
2024-01-28 20:53:06 -0600

    testing running app standalone

--- BitFumes Tutorial/main.py
+++ BitFumes Tutorial/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel
+import uvicorn
 
 
 app = FastAPI()
@@ -31,8 +32,11 @@ def comments(id, limit=10):
 class Blog(BaseModel):
     title: str 
     body: str 
-    published_at: Optional[bool]
+    published: Optional[bool]
 
 @app.post('/blog')
 def create_blog(request: Blog):
     return {'data': f'Blog is created with title as {request.title}'}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=9000)
