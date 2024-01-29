caf83a4fe3ebb2fba7303b9c1c2a6511ec5d25ba
Adam Camerer <ajc3xc@umsystem.edu>
2024-01-28 20:12:18 -0600

    finished request body

--- BitFumes Tutorial/main.py
+++ BitFumes Tutorial/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from typing import Optional
+from pydantic import BaseModel
+
+
 app = FastAPI()
 
 @app.get('/blog')
@@ -22,5 +25,14 @@ def show(id: int):
 
 @app.get('/blog/{id}/comments')
 def comments(id, limit=10):
-    
     return {'data': {'1', '2'}}
+
+
+class Blog(BaseModel):
+    title: str 
+    body: str 
+    published_at: Optional[bool]
+
+@app.post('/blog')
+def create_blog(request: Blog):
+    return {'data': f'Blog is created with title as {request.title}'}
