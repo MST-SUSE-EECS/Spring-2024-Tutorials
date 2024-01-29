bd774e2153ddfd1592791cc636e1352e13fbc0ce
Adam Camerer <ajc3xc@umsystem.edu>
2024-01-28 21:59:22 -0600

    connected program to database

--- BitFumes Tutorial/blog/main.py
+++ BitFumes Tutorial/blog/main.py
@@ -1,11 +1,12 @@
-from fastapi import FastAPI 
-from pydantic import BaseModel
+from fastapi import FastAPI
+from . import schemas, models
+from .database import engine
 app = FastAPI()
 
-class Blog(BaseModel):
-    title: str
-    body: str 
+
+models.Base.metadata.create_all(engine)
+
 
 @app.post('/blog')
-def create(request: Blog):
+def create(request: schemas.Blog):
     return request
