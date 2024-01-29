05a5c729919ff8f347aebf687e0ab57fb60b89d3
Adam Camerer <ajc3xc@umsystem.edu>
2024-01-28 18:00:38 -0600

    finished path parameters section

--- BitFumes Tutorial/main.py
+++ BitFumes Tutorial/main.py
@@ -4,8 +4,18 @@ app = FastAPI()
 
 @app.get('/')
 def index():
-    return {"data": {"name": "Adam"}}
+    return {"data": 'blog list'}
 
-@app.get('/about')
-def about():
-    return {'data': 'about page'}
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all unpublished blogs'}
+
+@app.get('/blog/{id}')
+def show(id: int):
+    return {'data': id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id: int):
+    return {'data': {'1', '2'}}
